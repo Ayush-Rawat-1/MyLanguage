857e526e94c408f6e7206357f2344e4fea5c0c17
Ayush Rawat <rawatayush.uki@gmail.com>
2025-03-13 10:23:33 +0530

    Lexer initial

---
+++ com/mylanguage/Hapi.java
@@ -0,0 +1,50 @@
+package com.mylanguage;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Hapi {
+    private static boolean hadError = false;
+    // run file
+    private static void runFile(String path) throws IOException{
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes,Charset.defaultCharset()));
+
+        // indicate an error in exit code
+        if(hadError){
+            System.exit(65);
+        }
+    }
+
+    private static void run(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for(Token token : tokens){
+            System.out.println(token);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if(args.length == 1){
+            runFile(args[0]);
+        }else{
+            System.err.println("Use jhapi script");
+            System.exit(64);
+        }
+    }
+
+    // error handling
+    protected static void error(int line, String message){
+        report(line,"",message);
+    }
+
+    private static void report(int line,String where,String message){
+        System.err.println("[line "+line+" ] Error "+where+" : "+message);
+        hadError=true;
+    }
+
+}
---
+++ com/mylanguage/Scanner.java
@@ -0,0 +1,107 @@
+package com.mylanguage;
+
+import java.util.ArrayList;
+import java.util.List;
+import static com.mylanguage.TokenType.*;
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens;
+    private int start=0;
+    private int current=0;
+    private int line=1;
+
+    Scanner(String source){
+        this.source=source;
+        this.tokens=new ArrayList<>();
+    }
+
+    public List<Token> scanTokens(){
+        while(!isAtEnd()){
+            // We are at the beginning of the next lexeme.
+            start=current;
+            advance();
+            scanToken();
+        }
+        tokens.add(new Token(EOF,"",null,line));
+        return tokens;
+    }
+
+    private boolean isAtEnd(){
+        return this.current >= this.source.length();
+    }
+
+    private char advance(){
+        this.current++;
+        return this.source.charAt(this.current-1);
+    }
+
+    private void scanToken(){
+        char c=advance();
+        switch (c){
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if (match('/')) {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+            // Ignore whitespace.
+            case ' ': break;
+            case '\r': break;
+            case '\t': break;
+            case '\n':
+                line++;
+                break;
+
+            default:
+                Hapi.error(line,"Unexpected character.");
+        }
+    }
+
+    private void addToken(TokenType type){
+        addToken(type,null);
+    }
+
+    private void addToken(TokenType type,Object literal){
+        String lexeme = this.source.substring(start,current);
+        this.tokens.add(new Token(type,lexeme,literal,this.line));
+    }
+
+    private boolean match(char expected){
+        if(isAtEnd()) return false;
+        if(this.source.charAt(this.current) != expected) return false;
+
+        this.current++;
+        return true;
+    }
+
+    private char peek(){
+        if(isAtEnd()) return '\0'; // null character
+        return this.source.charAt(this.current);
+    }
+
+}
