dd471e6347fa4d3cc74e0e783ca87b1e673deda3
Ayush Rawat <rawatayush.uki@gmail.com>
2025-05-16 22:10:22 +0530

    Evaluating Expression

--- com/mylanguage/Hapi.java
+++ com/mylanguage/Hapi.java
@@ -1,50 +1,85 @@
 package com.mylanguage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class Hapi {
-    private static boolean hadError = false;
-    // run file
-    private static void runFile(String path) throws IOException{
-        byte[] bytes = Files.readAllBytes(Paths.get(path));
-        run(new String(bytes,Charset.defaultCharset()));
+    static boolean errorOccured = false;
+    static boolean runtimeError = false;
 
-        // indicate an error in exit code
-        if(hadError){
-            System.exit(65);
+    private static final Interpreter interpreter = new Interpreter();
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jail [script]");
+            System.exit(64);     
+        }
+        else if (args.length == 1) {
+            runFile(args[0]);    
+        }
+        else {
+            runPrompt();
         }
     }
 
-    private static void run(String source){
+    //TO run a file present at path
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        // Indicate an error in the exit code.
+        if (errorOccured) System.exit(65);
+        if(runtimeError) System.exit(70);
+    }
+
+    //To reun the code from command Line
+    private static void runPrompt() throws IOException {
+        System.out.println("!!!!!Welcome TO jail");
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) 
+                break;
+            run(line);
+            errorOccured = false;
+        }
+    }
+    private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-
-        for(Token token : tokens){
-            System.out.println(token);
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+        System.out.println(tokens);
+        System.out.println(new AstPrinter().print(expression));
+        // Stop if there was a syntax error.
+        if (errorOccured) return;
+        interpreter.interpret(expression);
+    }
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+            "\n[line " + error.token.line + "]");
+        runtimeError = true;
+    }
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+    private static void report(int line, String where,String message) {
+        System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+        errorOccured = true;
+    }
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } 
+        else {
+            report(token.line, " at '" + token.lexeme + "'", message);
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        if(args.length == 1){
-            runFile(args[0]);
-        }else{
-            System.err.println("Use jhapi file_name.hapi");
-            System.exit(64);
-        }
-    }
-
-    // error handling
-    protected static void error(int line, String message){
-        report(line,"",message);
-    }
-
-    private static void report(int line,String where,String message){
-        System.err.println("[line "+line+" ] Error "+where+" : "+message);
-        hadError=true;
     }
 
 }
--- com/mylanguage/Scanner.java
+++ com/mylanguage/Scanner.java
@@ -1,65 +1,44 @@
 package com.mylanguage;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
 import static com.mylanguage.TokenType.*;
+import java.util.ArrayList;
+import java.util.List; 
 
 class Scanner {
     private final String source;
-    private final List<Token> tokens;
-    private int start=0;
-    private int current=0;
-    private int line=1;
-    private static final Map<String, TokenType> keywords;
-    static {
-        keywords = new HashMap<>();
-        keywords.put("and", AND);
-        keywords.put("class",  CLASS);
-        keywords.put("else", ELSE);
-        keywords.put("false",  FALSE);
-        keywords.put("for", FOR);
-        keywords.put("fun",FUN);
-        keywords.put("if", IF);
-        keywords.put("nil", NIL);
-        keywords.put("or", OR);
-        keywords.put("print",  PRINT);
-        keywords.put("return", RETURN);
-        keywords.put("super",  SUPER);
-        keywords.put("this",THIS);
-        keywords.put("true", TRUE);
-        keywords.put("var", VAR);
-        keywords.put("while",  WHILE);
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+    Scanner(String source) {
+        this.source = source;
     }
-
-    Scanner(String source){
-        this.source=source;
-        this.tokens=new ArrayList<>();
-    }
-
-    public List<Token> scanTokens(){
-        while(!isAtEnd()){
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
             // We are at the beginning of the next lexeme.
-            start=current;
+            start = current;
             scanToken();
         }
-        tokens.add(new Token(EOF,"",null,this.line));
+        tokens.add(new Token(EOF, "", null, line));
         return tokens;
     }
-
-    private boolean isAtEnd(){
-        return this.current >= this.source.length();
+    private boolean isAtEnd() {
+        return current >= source.length();
     }
-
-    private char advance(){
-        this.current++;
-        return this.source.charAt(this.current-1);
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
     }
-
-    private void scanToken(){
-        char c=advance();
-        switch (c){
+        private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+        private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
             case '(': addToken(LEFT_PAREN); break;
             case ')': addToken(RIGHT_PAREN); break;
             case '{': addToken(LEFT_BRACE); break;
@@ -69,7 +48,7 @@ class Scanner {
             case '-': addToken(MINUS); break;
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
-            case '*': addToken(STAR); break;
+            case '*': addToken(STAR); break; 
             case '!':
                 addToken(match('=') ? BANG_EQUAL : BANG);
                 break;
@@ -84,106 +63,88 @@ class Scanner {
                 break;
             case '/':
                 if (match('/')) {
-                    // A comment goes until the end of the line.
+                // A comment goes until the end of the line.
                     while (peek() != '\n' && !isAtEnd()) advance();
-                } else {
+                 } 
+                       
+                    else 
                     addToken(SLASH);
-                }
-                break;
-            // Ignore whitespace.
-            case ' ': break;
-            case '\r': break;
-            case '\t': break;
-            case '\n':
-                this.line++;
-                break;
-            case '"': string(); break;
-
-            default:
-                if(isDigit(c)){
-                    number();
-                } else if(isAlpha(c)){
-                    identifier();
-                } else {
-                    Hapi.error(this.line,"Unexpected character.");
-                }
+                    break;
+                case ' ':
+                case '\r':
+                case '\t':
+                // Ignore whitespace.
+                    break;
+                case '\n':line++;
+                    break;
+                case '"':string();
+                    break;
+                default:
+                    if (isDigit(c)) {
+                      number();
+                    } 
+                    else if (isAlpha(c)) {
+                        identifier();
+                    } else {
+                      Hapi.error(line, "Unexpected character.");
+                    }
         }
     }
-
-
-    private void addToken(TokenType type){
-        addToken(type,null);
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
     }
-
-    private void addToken(TokenType type,Object literal){
-        String lexeme = this.source.substring(start,current);
-        this.tokens.add(new Token(type,lexeme,literal,this.line));
-    }
-
-    private boolean match(char expected){
-        if(isAtEnd()) return false;
-        if(this.source.charAt(this.current) != expected) return false;
-
-        this.current++;
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+        current++;
         return true;
     }
-
-    private char peek(){
-        if(isAtEnd()) return '\0'; // null character
-        return this.source.charAt(this.current);
-    }
-
-    private char peekNext() {
-        if (current + 1 >= source.length()) return '\0';
-        return source.charAt(current + 1);
-    }
-
-    private void string(){
-        while(peek() != '"' && !isAtEnd()){
-            if (peek() == '\n') this.line++;
-            advance();
-        }
-        if(isAtEnd()){
-            Hapi.error(line, "Unterminated string.");
-            return;
-        }
-        // closing ".
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n') line++;
+        advance();
+           }
+        if (isAtEnd()) {
+        Hapi.error(line, "Unterminated string.");
+        return;
+           }
+        // The closing ".
         advance();
         // Trim the surrounding quotes.
         String value = source.substring(start + 1, current - 1);
         addToken(STRING, value);
+        Character.isDigit('t');
     }
-
-    private boolean isDigit(char ch){
-        return ch >= '0' && ch <= '9';
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
-
     private void number() {
         while (isDigit(peek())) advance();
         // Look for a fractional part.
         if (peek() == '.' && isDigit(peekNext())) {
-            // Consume the "."
-            advance();
-            while (isDigit(peek())) advance();
-        }
-        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+        // Consume the "."
+        advance();
+        while (isDigit(peek())) advance();
+           }
+        addToken(NUMBER,
+        Double.parseDouble(source.substring(start, current)));
     }
-
-    private boolean isAlpha(char ch){
-        return (ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || ch == '_';
-    }
-
-    private boolean isAlphaNumeric(char ch){
-        return isAlpha(ch) || isDigit(ch);
-    }
-
-    private void identifier(){
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    } 
+    private void identifier() {
         while (isAlphaNumeric(peek())) advance();
         String text = source.substring(start, current);
-        TokenType type = keywords.get(text);
-        if (type == null) type = IDENTIFIER;
-
+        TokenType type = new Keywords().keywords.getOrDefault(text,IDENTIFIER);
         addToken(type);
     }
-
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||(c>= 'A' && c <= 'Z') ||c == '_';
+    }
+    private boolean isAlphaNumeric(char c){
+        return isAlpha(c) || isDigit(c);
+    }
+    
 }
